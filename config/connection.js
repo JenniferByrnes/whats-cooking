ddc898ce64f3d7e45840e66ac46b63ae99dcbe05
Jennifer Byrnes <jennifer.byrnes@outlook.com>
2022-09-03 18:57:35 -0400

    adding fields to front end

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,7 @@
 const Sequelize = require("sequelize");
 
 require("dotenv").config();
-console.log(
-  "=================================================",
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD
-);
+
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
