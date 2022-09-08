a1d1d21b5a862149e924d2adcac130b707bd8253
Camo282 <camo282@gmail.com>
2022-09-07 21:04:07 -0400

    updated changes

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,17 @@
-const Sequelize = require("sequelize");
+const Sequelize = require('sequelize');
 
-require("dotenv").config();
+require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  // "recipe_db",
-  // "root",
-  // "586491",
-  {
-    host: "localhost",
-    dialect: "mysql",
-    port: 3306,
-    dialectOptions: {
-      decimalNumbers: true,
-    },
-  }
-);
+// create connection to our db
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306,
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+    });
 
 module.exports = sequelize;
